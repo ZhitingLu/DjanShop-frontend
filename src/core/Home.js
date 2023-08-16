ddcbd45d0880d68535d5979d08182203438cbb83
Zhiting Lu <zhitinglu@outlook.com>
2023-08-16 18:44:03 +0200

    getting all product names

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { getProducts } from "./helper/coreapicalls";
 
 export default function Home() {
+
+    const [products, setProducts] = useState([]);
+    const [error, setError] = useState(false);
+
+    const loadAllProducts = () => {
+        getProducts()
+            .then(data => {
+                if (data.error) {
+                    setError(data.error);
+                    console.log(data.error);
+                }
+                else {
+                    setProducts(data);
+                }
+            })
+            .catch(err => console.log(err));
+    }
+
+
+    useEffect(() => {
+        loadAllProducts();
+    }, []);
+
     return (
         <div>
-        <h1>Home component</h1>
+            <h1>Home component</h1>
+            <div className='Row'>
+                {products.map((product, index) => {
+                    return (
+                        <div key={index}>
+                            <h1>{product.name}</h1>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
