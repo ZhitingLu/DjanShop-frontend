bb77706c2864752be0b4f782fe10447bbbbd8487
Zhiting Lu <zhitinglu@outlook.com>
2023-08-17 13:55:22 +0200

    upgraded react dom router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import './styles.css';
 import Routes from './MyRoutes';
 
-ReactDOM.render(<Routes />, document.getElementById("root"));
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(<Routes />);
