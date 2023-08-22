d81a475a787105fcef3a20dd1c21b65c2afc8d0c
Zhiting Lu <zhitinglu@outlook.com>
2023-08-22 12:02:45 +0200

    fixed bug

--- src/core/Home.js
+++ src/core/Home.js
@@ -36,7 +36,7 @@ const Home = () => {
                 {products.map((product, index) => {
                     return (
                         <div key={index} className='col-4 mb-4'>
-                           <Card product={product} />
+                            <Card product={product} />
                         </div>
                     )
                 })}
