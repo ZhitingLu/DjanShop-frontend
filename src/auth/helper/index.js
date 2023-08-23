d21ae812eb1d22179855b05208ebc299ea0fff2a
Zhiting Lu <zhitinglu@outlook.com>
2023-08-23 12:41:25 +0200

    signin and signup helper

---
+++ src/auth/helper/index.js
@@ -0,0 +1,34 @@
+import { API } from "../../backend";
+import { emptyCart } from "../../core/helper/cartHelper";
+
+export const signup = (user) => {
+    return fetch(`${API}user/`, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user)
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+}
+
+export const signin = (user) => {
+    const formData = new FormData();
+
+    for (const name in user) {
+        formData.append(name, user[name]);
+    }
+
+    return fetch(`${API}user/login/`, {
+        method: "POST",
+        body: formData
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+}
