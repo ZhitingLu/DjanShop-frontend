6da9e657cf0bcc87aa1b8728a65ffbf471304149
Zhiting Lu <zhitinglu@outlook.com>
2023-08-24 09:28:30 +0200

    handling private route

--- src/MyRoutes.js
+++ src/MyRoutes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./core/Home";
+import PrivateRoutes from "./auth/helper/PrivateRoutes";
 
 const MyRoutes = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+                {/* <PrivateRoutes path="/user/dashboard" exact element={} /> */}
             </Routes>
         </BrowserRouter>
     )
