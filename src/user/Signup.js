80758cc6869ac0665439e1495ed9497186772c17
Zhiting Lu <zhitinglu@outlook.com>
2023-08-28 16:50:03 +0200

    created signup form template

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,10 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Base from '../core/Base';
+import { Link } from 'react-router-dom';
+import { signup } from '../auth/helper';
 
 const Signup = () => {
+    const [values, setValues] = useState({
+        name: "",
+        email: "",
+        password: "",
+        error: "",
+        success: false,
+    })
+
+    const { name, email, password, error, success } = values;
+
+    // higher order function
+    const handleChange = name => event => {
+        setValues({ ...values, error: false, [name]: event.target.value });
+    }
+
+    const signupForm = () => {
+        return <div className='row'>
+            <div className='col-md-6 offset-sm-3 text-left'>
+                <form>
+                    <div className='form-group mb-2'>
+                        <label className='text-light mt-1'>Name</label>
+                        <input
+                            className='form-control'
+                            value={name}
+                            onChange={handleChange("name")}
+                            type="text" />
+                    </div>
+                    <div className='form-group mb-2'>
+                        <label className='text-light'>Email</label>
+                        <input
+                            className='form-control mt-1'
+                            value={email}
+                            onChange={handleChange("email")}
+                            type="text" />
+                    </div>
+                    <div className='form-group'>
+                        <label className='text-light'>Password</label>
+                        <input
+                            className='form-control'
+                            value={password}
+                            onChange={handleChange("password")}
+                            type="password" />
+                    </div>
+                    <button className='btn btn-success btn-block mt-3'>Submit</button>
+                </form>
+            </div>
+        </div>
+    }
+
     return (
         <Base title="Sign Up Page" description='A signup for user' >
-            <p>Test of signup page</p>
+            {signupForm()}
         </Base>
     )
 }
