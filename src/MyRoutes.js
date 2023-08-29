31aa2515d3a8f4f36965363649133c27869228cc
Zhiting Lu <zhitinglu@outlook.com>
2023-08-29 11:26:12 +0200

    setup route for signin

--- src/MyRoutes.js
+++ src/MyRoutes.js
@@ -5,6 +5,8 @@ import Home from "./core/Home";
 import PrivateRoutes from "./auth/helper/PrivateRoutes";
 import Signup from "./user/Signup";
 import UserDashboard from "./user/UserDashboard";
+import Signin from "./user/Signin";
+
 
 const MyRoutes = () => {
     return (
@@ -12,8 +14,10 @@ const MyRoutes = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/signup" element={<Signup />} />
+                <Route path="/signin" element={<Signin />} />
                 {/* Wrap PrivateRoutes around the UserDashboard Route */}
                 <Route path="/user/dashboard" element={<PrivateRoutes component={UserDashboard} />} />
+                {/* <PrivateRoutes path="/user/dashboard" element={UserDashboard} /> */}
                  
             </Routes>
         </BrowserRouter>
---
+++ src/user/Signin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Base from '../core/Base';
+
+
+const Signin = () => {
+    return (
+        <Base title='Welcome to signin page' description='My store'>
+        </Base>
+    )
+}
+
+export default Signin;
