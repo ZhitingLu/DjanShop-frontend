2815aec47780e4f84f8bfb7c908a6f10bcf53fce
Zhiting Lu <zhitinglu@outlook.com>
2023-08-29 14:55:30 +0200

    Getting the form ready for signin

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,10 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Base from '../core/Base';
-
+import { Link } from 'react-router-dom';
 
 const Signin = () => {
+
+    const [values, setValues] = useState({
+        name: "",
+        email: "",
+        password: "",
+        error: "",
+        success: false,
+        loading: false,
+        didRedirect: false
+    })
+
+    const { name, email, password, error, success, loading, didRedirect } = values;
+
+    // higher order function
+    const handleChange = name => event => {
+        setValues({
+            ...values,
+            error: false,
+            [name]: event.target.value
+        });
+    }
+
+    const successMessage = () => {
+        return (
+            <div className='row'>
+                <div className='col-md-6 offset-sm-3 text-left'>
+                    <div className='alert alert-success'
+                        style={{ display: success ? "" : "none" }}>
+                        New account created successfully. Please <Link to="/signin">login now.</Link>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    const errorMessage = () => {
+        return (
+            <div className='row'>
+                <div className='col-md-6 offset-sm-3 text-left'>
+                    <div className='alert alert-danger'
+                        style={{ display: error ? "" : "none" }}>
+                        Check all fields again
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    const signinForm = () => {
+        return <div className='row'>
+            <div className='col-md-6 offset-sm-3 text-left'>
+                <form>
+                    <div className='form-group mb-2'>
+                        <label className='text-light'>Email</label>
+                        <input
+                            className='form-control mt-1'
+                            value={email}
+                            onChange={handleChange("email")}
+                            type="text" />
+                    </div>
+                    <div className='form-group'>
+                        <label className='text-light'>Password</label>
+                        <input
+                            className='form-control'
+                            value={password}
+                            onChange={handleChange("password")}
+                            type="password" />
+                    </div>
+                    <button
+                        // onClick={}
+                        type="button"
+                        className='btn btn-success w-100 mt-3'>
+                        Submit
+                    </button>
+                </form>
+            </div>
+        </div>
+    }
+
     return (
         <Base title='Welcome to signin page' description='My store'>
+            {signinForm()}
+            <p className='text-center'>
+                {JSON.stringify(values)}
+            </p>
         </Base>
     )
 }
